fe4916db33e1f2bedc1e27321a5a943c0af9d73a
dragiev1 <dragievs2020@gmail.com>
2025-11-12 16:57:27 -0500

    fix: can now connect to databases securely

--- server/src/main/java/com/calcmadeeasy/CalcMadeEasyApplication.java
+++ server/src/main/java/com/calcmadeeasy/CalcMadeEasyApplication.java
@@ -10,6 +10,11 @@ import com.calcmadeeasy.repository.ProblemRepo;
 import com.calcmadeeasy.repository.TagRepo;
 import com.calcmadeeasy.repository.UserRepo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
@@ -18,6 +23,24 @@ public class CalcMadeEasyApplication {
         public static void main(String[] args) {
                 SpringApplication.run(CalcMadeEasyApplication.class, args);
 
+        }
+
+        @Bean
+        CommandLineRunner testDatasource(UserRepo userRepo, CourseRepo courseRepo, PageRepo pageRepo, ProblemRepo problemRepo, TagRepo tagRepo, DataSource datasource) {
+                return args -> {
+                        System.out.println("---- Testing Database Connection ----"); 
+
+                        try (Connection conn = datasource.getConnection()) {
+                                 System.out.println("Connected to database");
+                                 System.out.println("Database Product Name: " + conn.getMetaData().getDatabaseProductName());
+                                 System.out.println("Database Product Version: " + conn.getMetaData().getDatabaseProductVersion());
+                        } catch (SQLException e) {
+                                System.err.println("Failed to connect to database!");
+                                e.printStackTrace();
+                        }
+
+                        System.out.println("---- End of Test ----");
+                };
         }
 
         @Bean
