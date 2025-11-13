88c23897d2656231041b221bee114d69d1bdde28
dragiev1 <dragievs2020@gmail.com>
2025-11-13 10:44:42 -0500

    feat: created a DTO object for requests requiring sending Users

--- server/src/main/java/com/calcmadeeasy/dto/UserDTO.java
+++ server/src/main/java/com/calcmadeeasy/dto/UserDTO.java
@@ -1,23 +1,77 @@
 package com.calcmadeeasy.dto;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.calcmadeeasy.models.Users.User;
+import com.calcmadeeasy.models.Users.UserProgress;
 
+/*
+ * Data Transfer Object for all/single User requests. 
+ */
 public class UserDTO {
   private UUID id;
   private String firstName;
   private String lastName;
   private String email;
   private String profilePicUrl;
+  private List<UserProgress> up;
 
-  public UserDTO(User user) {
+  public UserDTO(User user, List<UserProgress> up) {
     this.id = user.getId();
     this.firstName = user.getFirstName();
     this.lastName = user.getLastName();
     this.email = user.getEmail();
     this.profilePicUrl = user.getProfilePicUrl();
+    this.up = up;
   }
 
-  // TODO: Add getters and setters.
+  // Getters
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getProfilePicUrl() {
+    return profilePicUrl;
+  }
+
+  public List<UserProgress> getUserProgress() {
+    return up;
+  }
+
+  // Setters
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setProfilePicUrl(String profilePicUrl) {
+    this.profilePicUrl = profilePicUrl;
+  }
+
+  public void setUserProgress(List<UserProgress> up) {
+    this.up = up;
+  }
+
 }
