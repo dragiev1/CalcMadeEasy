fc2dcbcc713b651ca441b0182632c5c543a5c666
dragiev1 <dragievs2020@gmail.com>
2025-12-01 12:01:35 -0500

    fix: made SectionDTO extend from SectionResponseDTO for simplicity

--- server/src/main/java/com/calcmadeeasy/dto/Sections/SectionDTO.java
+++ server/src/main/java/com/calcmadeeasy/dto/Sections/SectionDTO.java
@@ -1,57 +1,25 @@
 package com.calcmadeeasy.dto.Sections;
 
-import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 
 import com.calcmadeeasy.models.Pages.Page;
 import com.calcmadeeasy.models.Sections.Section;
 
 // Outbound only.
-public class SectionDTO {
-  private UUID id;
-  private String title;
-  private String description;
+public class SectionDTO extends SectionResponseDTO {
   private List<Page> pages;
-  private Instant createdAt;
-  private Instant updatedAt;
 
-  public SectionDTO() {
-  }
+  public SectionDTO() {}
 
   public SectionDTO(Section section) {
-    this.id = section.getId();
-    this.description = section.getDescription();
-    this.title = section.getTitle();
+    super(section);
     this.pages = section.getPages();
-    this.createdAt = section.getCreatedAt();
-    this.updatedAt = section.getUpdatedAt();
   }
 
   // Getters.
 
-  public UUID getId() {
-    return id;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
   public List<Page> getPages() {
     return pages;
-  }
-
-  public Instant getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public Instant getCreatedAt() {
-    return createdAt;
   }
 
 }
