5beae708d3dad9106bb5ac9325c748e65d8f4902
dragiev1 <dragievs2020@gmail.com>
2026-03-19 13:37:21 -0400

    feat: inserted the full_logo in the home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar";
+import "../css/Home.css";
 
 function Home() {
   return ( 
     <>
       <Navbar />
 
-      
+      <div className="hero-container">
+        <img src={'src/assets/full_logo.svg'} className="full-logo" />
+        <div className="animation-player">ANIMATION</div>
+      </div>
     </>
   );
 };
