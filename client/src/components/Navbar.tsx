aca8eaed943b17e8060a64b4b68e1f102f1d7803
dragiev1 <dragievs2020@gmail.com>
2026-03-19 11:54:28 -0400

    feat: made global colors

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 function Navbar() {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="navbar-items">
+        <Link to={"/"} className="nav-text nav-link">Icon</Link>
+        <Link to={"/dashboard"} className="nav-text nav-link">Learn</Link>
+      </div>
+    </div>
   )
 }
 
