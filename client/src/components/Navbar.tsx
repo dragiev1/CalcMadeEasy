b05d048bb1bad54fd673b66a3955cdc4d3b8c9b0
dragiev1 <dragievs2020@gmail.com>
2026-03-19 13:37:38 -0400

    feat: added the CME logo to the navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
+import logo from "../assets/logo.svg";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar-items">
-        <Link to={"/"} className="nav-text nav-link">Icon</Link>
-        <Link to={"/dashboard"} className="nav-text nav-link">Learn</Link>
+        <Link to={"/"} className="nav-text nav-link">
+          <img src={logo} alt="logo" className="logo" />
+        </Link>
+        <Link to={"/dashboard"} className="nav-text nav-link">Google</Link>
       </div>
     </div>
   )
