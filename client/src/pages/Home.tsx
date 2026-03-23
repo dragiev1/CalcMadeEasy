8a69037a4882a792d56dbef3920c860a6af390fc
dragiev1 <dragievs2020@gmail.com>
2026-03-23 14:43:35 -0400

    fix: divider in home under video player kept glitching upwards as the next animation rolls in

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,12 @@
 import Navbar from "../components/Navbar";
 import "../css/Home.css";
-import {useState} from "react";
+import { useState } from "react";
 
 import gaussianIntegral from "../assets/math-animations/GaussianIntegral.mp4";
 import riemannSums from "../assets/math-animations/RiemannSums.mp4";
 import lorenzAttractor from "../assets/math-animations/LorenzAttractorWithEquations.mp4";
 
-const mathAnimations = [
-    gaussianIntegral,
-    lorenzAttractor,
-    riemannSums,
-  ]
+const mathAnimations = [riemannSums, gaussianIntegral, lorenzAttractor];
 
 function Home() {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
@@ -19,35 +15,39 @@ function Home() {
     setCurrentVideoIndex((prev) => (prev + 1) % mathAnimations.length);
   };
 
-  return ( 
+  return (
     <>
       <Navbar />
 
       <div className="hero-container">
         <div className="full-logo-container">
-          <img src={'src/assets/full_logo.svg'} className="full-logo" alt="CalcMadeEasy Logo" ></img>
+          <img
+            src={"src/assets/full_logo.svg"}
+            className="full-logo"
+            alt="CalcMadeEasy Logo"
+          ></img>
           <div className="subtitle">Where math clicks. </div>
         </div>
-        
+
         <div className="animation-player">
-          <video 
+          <video
             key={currentVideoIndex}
             src={mathAnimations[currentVideoIndex]}
             autoPlay
             muted
             playsInline
-            className="lorenz-animation" 
+            className="lorenz-animation"
             onEnded={handleVideoEnd}
-          > Your browser does not support the video tag.
+          >
+            {" "}
+            Your browser does not support the video tag.
           </video>
         </div>
       </div>
 
-
       <div className="divider" />
-      
     </>
   );
-};
+}
 
 export default Home;
